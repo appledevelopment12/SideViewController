b2c18357a1965651b95cd9dd261fab0401f68966
rk <kumar.rohit512000@gmail.com>
2023-10-27 21:37:55 +0530

    Initial Commit

---
+++ SideViewController/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SideViewController
+//
+//  Created by Rajeev on 27/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
